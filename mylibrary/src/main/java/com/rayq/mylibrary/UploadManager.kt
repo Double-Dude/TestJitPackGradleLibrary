a6487623702a533b03cbbc39ea390d78e655a1a6
DoubleDude <>
2021-12-14 01:03:52 +1300

    mylibrary uses mylibrary2

--- mylibrary/src/main/java/com/rayq/mylibrary/UploadManager.kt
+++ mylibrary/src/main/java/com/rayq/mylibrary/UploadManager.kt
@@ -1,9 +1,9 @@
 package com.rayq.mylibrary
 
-//import com.rayq.mylibrary2.MyLibrary2Test
+import com.rayq.mylibrary2.MyLibrary2Test
 
 class UploadManager {
     fun upload() {
-//        MyLibrary2Test().test()
+        MyLibrary2Test().test()
     }
 }
