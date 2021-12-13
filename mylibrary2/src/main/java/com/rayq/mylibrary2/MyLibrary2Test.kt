a410244379590735d4cd6946bf60276ace90f377
DoubleDude <>
2021-12-14 01:11:42 +1300

    Commented out MyLibrary2 function

--- mylibrary2/src/main/java/com/rayq/mylibrary2/MyLibrary2Test.kt
+++ mylibrary2/src/main/java/com/rayq/mylibrary2/MyLibrary2Test.kt
@@ -3,9 +3,9 @@ package com.rayq.mylibrary2
 import android.util.Log
 
 class MyLibrary2Test {
-    fun test() {
-        test1()
-    }
+//    fun test() {
+//        test1()
+//    }
 
     private fun test1() {
         Log.d("MyLibrary2Test", "Test1 is called")
