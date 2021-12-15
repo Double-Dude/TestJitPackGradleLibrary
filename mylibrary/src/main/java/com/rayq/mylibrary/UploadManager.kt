f6337fd1dcd55e92d0bdb0b2bdabb8dd30abeb03
DoubleDude <>
2021-12-15 15:13:33 +1300

    Use Dependency Injection

--- mylibrary/src/main/java/com/rayq/mylibrary/UploadManager.kt
+++ mylibrary/src/main/java/com/rayq/mylibrary/UploadManager.kt
@@ -2,8 +2,8 @@ package com.rayq.mylibrary
 
 import com.rayq.mylibrary2.MyLibrary2Test
 
-class UploadManager {
+class UploadManager(private val testClass: MyLibrary2Test) {
     fun upload() {
-        MyLibrary2Test().test()
+        testClass.test()
     }
 }
